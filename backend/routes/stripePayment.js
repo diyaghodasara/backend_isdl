193a62988c9bb3d8d8f25591fe82a75328ba0d7b
Diya Ghodasara <127593045+diyaghodasara@users.noreply.github.com>
2023-12-07 16:08:34 +0530

    Delete backend directory

--- backend/routes/stripePayment.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const paymentController = require("../controllers/paymentController");
-//const {isAdmin, isAuthenticated, isSignedIn} = require("../controllers/auth");
-
-router.post("/stripePayment", paymentController.makePayment);
-
-module.exports = router;
